24ac9faca003f8d9d7cd88edef26a3e3e0df73d4
JottaTech <157697139+SOUZA-BEEP@users.noreply.github.com>
2025-09-18 15:46:08 +0100

    Add files via upload

---
+++ Primeiroprogama/src/PrimeiroProgama.java
@@ -0,0 +1,32 @@
+public class PrimeiroProgama{
+    public static void main (String []args){
+        String nome = "luccas" ;
+        String morada = "new york";
+        int idade = 22;
+         // casado, solteiro, viuvu , dix
+        char estado_civil = 's';
+
+
+        System.out.println("Nome:"+ nome);
+        System.out.println("morada:"+ morada);
+        System.out.println("idade:"+ idade);
+        System.out.println("estado_civil:"+ estado_civil);
+
+        System.out.println("nome:"+ nome + " , morada:"+ morada +", idade:"+ idade + ", estado_civil:"+ estado_civil);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
