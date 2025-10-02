d0e2710ed7b6ca063bc6a9525f7def25b02b149f
JottaTech <157697139+SOUZA-BEEP@users.noreply.github.com>
2025-10-02 16:28:19 +0100

    Add files via upload

---
+++ ATIVIDADE_6/src/ATIVIDADE_6.java
@@ -0,0 +1,13 @@
+public class ATIVIDADE_6 {
+     public static void main (String[] args){
+
+         int valor = 5;
+         int jpg = 10;
+         String maiormenor = (jpg < valor) ? "maior" : "menor";
+         System.out.println(maiormenor);
+
+         String diferente=jpg%valor==0 ? "vdd" : "fss";
+         System.out.println(diferente);
+
+     }
+}
