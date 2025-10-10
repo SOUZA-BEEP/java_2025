1572009c1f23645c1b50951e857391574a5b3671
JottaTech <157697139+SOUZA-BEEP@users.noreply.github.com>
2025-10-10 09:31:50 +0100

    Add files via upload

---
+++ Atividade13.java
@@ -0,0 +1,11 @@
+public class Atividade13 {
+    public static void main(String[] args){
+
+        int agua = 100 ;
+
+        if(agua>=100)
+            System.out.println("agua esta a fever");
+        else System.out.println("agua nao fever");
+    }
+
+}
