17a330cc2e895ed6dcd08fe7664da42eabf67dd3
JottaTech <157697139+SOUZA-BEEP@users.noreply.github.com>
2026-01-23 15:42:39 +0000

    Add files via upload

---
+++ Ex027.java
@@ -0,0 +1,12 @@
+public class Ex027 {
+    public static void main(String[] args){
+        String idade = "30";
+        System.out.println("valor str:" + idade);
+        int idade2 = Integer.parseInt(idade);
+        System.out.println("valor str:" + idade2);
+
+        float idade3 = Float.parseFloat(idade);
+        System.out.println("valor str:" + idade);
+    }
+
+}
